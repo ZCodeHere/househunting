2af5926f9f0920069bdb5eaac90079f386e465ca
JIEYUN FU <jieyun.fu @jpmorgan.com>
2015-05-18 16:49:06 -0400

    zip code initialization

--- main/main.py
+++ main/main.py
@@ -1,7 +1,33 @@
-#!/usr/bin/python
+import os
+from util import ZipCode
+
 
 def main():
-    pass; 
+    initZipCode()
+
+def initZipCode():
+    dir = os.path.dirname(os.path.realpath(__file__))
+    zipCodeFile = dir + '\..\data\NJ_zipcode.txt'
+    
+    zipCodeList = []
+    
+    with open(zipCodeFile) as f:
+        while True:
+            line = f.readline()
+            line = line.trim()
+            if (len(line) == 0):
+                break
+            
+            arr = line.split('\t')
+            zipcode = arr[1]
+            town = arr[2]
+            longtitude = float(arr[-2])
+            latitude = float(arr[-1])
+            zipCodeObj = ZipCode(zipcode, town, longtitude, latitude)
+            
+            zipCodeList.append(zipCodeObj)
+
+    return zipCodeList
 
 if __name__ == '__main__':
-    main();
+    main()
--- main/util.py
+++ main/util.py
@@ -3,23 +3,48 @@ class CityTaxRates():
     
     def __init__(self):
         self.map = {}
+        self.city_list = set()
     
     def load(self, filename):
         with open(filename) as f:
             st = f.readline()
-            st = st.trim();
+            st = st.trim()
             if len(st) != 0:
                 arr = st.split(' ')
                 rate = float(arr[0])
                 city_name = arr[0:-2]
                 self.map[city_name] = rate
+                self.city_list.add(city_name)
     
-    def getCityTaxRate(self, city):
-        return self.map[city]
-
-
-def get_city_from_zipcode(zipcode):
-    pass; 
+    def getCityTaxRate(self, city, approximateName = True):
+        bestMatch = self.getBestMatchName(city)
+        return self.map[bestMatch]
+    
+    def getBestMatchName(self, name):
+        bestScore = 0
+        bestMatch = None
+        for city in self.city_list:
+            score = self.matchScore(city, name)
+            if score > bestScore:
+                bestScore = score
+                bestMatch = city
+        return bestMatch
+             
+    def matchScore(self, name1, name2):
+        ans = 0
+        for i in xrange(min(len(name1), len(name2))):
+            if (name1[i] == name2[i]):
+                ans = i
+        return ans
+    
+    
+class ZipCode():
+    def __init__(self, zipCode, cityName, longtitude, latitude):
+        self.longtitude = longtitude
+        self.latitude = latitude
+        self.cityName = cityName
+        self.zipCode = zipCode
+        
 
 
     
